c0f8d673f8668e50daf9a492b9480205d2fcb5ab
lucasjdelri0 <lucasjdelrio@gmail.com>
2022-08-31 02:42:18 +0200

    Include key to dynamic radio group

--- src/components/survey/DailySurvey/_components/SurveyQuestion/SurveyQuestion.tsx
+++ src/components/survey/DailySurvey/_components/SurveyQuestion/SurveyQuestion.tsx
@@ -10,7 +10,7 @@ export const SurveyQuestion = ({
 }: SurveyQuestionProps): JSX.Element => (
   <>
     <Title level={4}>{question?.text}</Title>
-    <Radio.Group onChange={onAnswer} value={value}>
+    <Radio.Group onChange={onAnswer} value={value} key={question?.id}>
       <Space direction='vertical'>
         {question?.options.map(({ id, text }) => (
           <Radio value={id} key={id}>
